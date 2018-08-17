42c423f2cf806a05a61a67d3489227ac0f548ed6
Lucio Di Giacomo Noack <lucio.digiacomo@slyt.gba.gov.ar>
2018-08-17 13:51:45 -0300

    added background

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,11 @@ module TheraWebapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.x.application_name = 'Thera WebApp'
+    config.x.version = '0.1.0'
+    config.time_zone = 'Buenos Aires'
+    config.i18n.default_locale =    :'es-AR'
+    config.i18n.available_locales = :'es-AR', :en
   end
 end
